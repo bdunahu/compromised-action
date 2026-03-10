c29c42af73cbc8c28fcd07a38b47700d2dcffded
bdunahu <bdunahu@operationnull.com>
2026-03-10 19:16:01 -0400

    record guix development environment

---
+++ channels.scm
@@ -0,0 +1,10 @@
+(list (channel
+       (name 'guix)
+       (url "https://codeberg.org/guix/guix")
+       (branch "master")
+       (commit "c3c2f3be04364e3f616bfcc38875b112bedbe901")
+       (introduction
+        (make-channel-introduction
+         "9edb3f66fd807b096b48283debdcddccfea34bad"
+         (openpgp-fingerprint
+          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
